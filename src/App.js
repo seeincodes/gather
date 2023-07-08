7462056533af20618faa2ef86c701a376d5c4c18
jennyg0 <jen2348@hotmail.com>
2023-07-08 14:54:21 +0200

    add swipe card component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import User from './User';
 import Match from './Match';
 import Calendar from './Calendar';
 import './App.css';
+import SwipeCardComponent from './SwipeCardComponent';
 
 function App() {
   // State for the list of users
   const [users, setUsers] = useState([]);
   // State for the list of matches
   const [matches, setMatches] = useState([]);
-  
+
   // Function to add a new user
   const addUser = (user) => {
     setUsers([...users, user]);
@@ -24,7 +25,7 @@ function App() {
   const findMatch = (user) => {
     // Matching algorithm based on distance, interests, values, and hobbies
     // This is a placeholder and should be replaced with your own algorithm
-    const match = users.find(u => u !== user);
+    const match = users.find((u) => u !== user);
     if (match) {
       addMatch(new Match(user, match));
     }
@@ -66,24 +67,26 @@ function App() {
 
   // Effect to schedule dates whenever a chat ends
   useEffect(() => {
-    matches.filter(m => m.chatEnded).forEach(scheduleDate);
+    matches.filter((m) => m.chatEnded).forEach(scheduleDate);
   }, [matches]);
 
   return (
-    <div id="app">
+    <div id='app'>
       {/* Display the list of users */}
-      <div id="users">
-        {users.map(user => (
+      {/* <div id='users'>
+        {users.map((user) => (
           <div key={user.name}>{user.name}</div>
         ))}
-      </div>
-
+      </div> */}
+      <SwipeCardComponent />
       {/* Display the list of matches */}
-      <div id="matches">
-        {matches.map(match => (
-          <div key={match.id}>{match.user1.name} matched with {match.user2.name}</div>
+      {/* <div id='matches'>
+        {matches.map((match) => (
+          <div key={match.id}>
+            {match.user1.name} matched with {match.user2.name}
+          </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -3,40 +3,40 @@ import React from 'react';
 
 // User class definition
 class User extends React.Component {
-    constructor(props) {
-        super(props);
-        // Initializing the state with the user details
-        this.state = {
-            name: props.name,
-            location: props.location,
-            interests: props.interests,
-            values: props.values,
-            hobbies: props.hobbies
-        };
-    }
+  constructor(props) {
+    super(props);
+    // Initializing the state with the user details
+    this.state = {
+      name: this.props.name,
+      location: this.props.location,
+      interests: this.props.interests,
+      values: this.props.values,
+      hobbies: this.props.hobbies,
+    };
+  }
 
-    // Function to update the user details
-    updateUserDetails = (details) => {
-        this.setState(details);
-    }
+  // Function to update the user details
+  updateUserDetails = (details) => {
+    this.setState(details);
+  };
 
-    // Function to get the user details
-    getUserDetails = () => {
-        return this.state;
-    }
+  // Function to get the user details
+  getUserDetails = () => {
+    return this.state;
+  };
 
-    // Rendering the user details
-    render() {
-        return (
-            <div>
-                <h2>{this.state.name}</h2>
-                <p>{this.state.location}</p>
-                <p>{this.state.interests}</p>
-                <p>{this.state.values}</p>
-                <p>{this.state.hobbies}</p>
-            </div>
-        );
-    }
+  // Rendering the user details
+  render() {
+    return (
+      <div>
+        <h2>{this.state.name}</h2>
+        <p>{this.state.location}</p>
+        <p>{this.state.interests}</p>
+        <p>{this.state.values}</p>
+        <p>{this.state.hobbies}</p>
+      </div>
+    );
+  }
 }
 
 // Exporting the User class
